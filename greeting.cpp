8026d816d998ab77d12fcbf18ba4697ac106e8cb
Haippp <haiperdesain@gmail.com>
2025-07-03 22:34:19 +0800

    Pengenalan Dasar

---
+++ greeting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nama, umur;
+
+  cout << "Masukkan nama: ";
+  cin >> nama;
+
+  scanf("%s", nama);
+  cout << "Halo " << nama << " Selamat datang di dunia coding";
+}
